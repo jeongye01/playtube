3071bf0d6452828de77003d69b028709bba3432d
yewon_jeong <yewon_jeong@tmax.co.kr>
2024-06-17 17:34:57 +0900

    Initial commit

---
+++ src/main/java/com/yewon/playtube/PlaytubeApplication.java
@@ -0,0 +1,13 @@
+package com.yewon.playtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaytubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaytubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yewon/playtube/PlaytubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yewon.playtube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaytubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
